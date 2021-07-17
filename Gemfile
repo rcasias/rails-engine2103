0d4efffd3a9b551ba448d732bc4896bd5732a40f
Regina Casias <reginaannvigil@gmail.com>
2021-07-16 19:58:08 -0600

    added functionality for index and show merchants

--- Gemfile
+++ Gemfile
@@ -31,6 +31,11 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'rspec-rails'
+  gem 'simplecov'
+  gem 'pry'
 end
 
 group :development do
@@ -38,10 +43,6 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-end
-
-group :test do
-  gem 'simplecov'
 end
 
 
---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of books" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+  end
+
+  it "sends a list of merchants" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to eq(3)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
+  end
+
+  it "can get one book by its id" do
+  id = create(:merchant).id
+
+  get "/api/v1/merchants/#{id}"
+
+  merchant = JSON.parse(response.body, symbolize_names: true)
+
+  expect(response).to be_successful
+
+  expect(merchant).to have_key(:id)
+  expect(merchant[:id]).to eq(id)
+
+  expect(merchant).to have_key(:name)
+  expect(merchant[:name]).to be_a(String)
+end
+end

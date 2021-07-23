5935374eadf1596c9f50e20902a606c0a25474e4
Regina Casias <reginaannvigil@gmail.com>
2021-07-22 18:27:07 -0600

    Added model testing to merchant- most items sold

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -25,24 +25,3 @@ class Merchant < ApplicationRecord
   end
 
 end
-
-
-
-# InvoiceItem.joins(:item).select('sum (invoice_items.quantity)', 'items.merchant_id').group('items.merchant_id').order('sum desc')
-#
-#
-# SELECT  sum (invoice_items.quantity) as count, merchants.*
-# FROM "merchants"
-# INNER JOIN
-# "items"
-# ON "merchants"."id" = "items"."merchant_id"
-# INNER JOIN "invoice_items"
-# ON "items"."id" = "invoice_items"."item_id"
-# Inner JOIN
-# "invoices"
-# on "invoices"."id" = "invoice_items"."invoice_id"
-# INNER JOIN
-# "transactions"
-# ON "transactions"."invoice_id" = "invoices"."id" and "transactions"."result" = 'success'
-# GROUP BY merchants.id
-# ORDER BY count desc;
